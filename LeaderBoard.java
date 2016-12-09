c4e50b0516d45ea15913a0954c22a4abdb2eb912
Sai Yarram <syarram@umass.edu>
2016-12-08 22:10:32 -0400

    Creating a new LeaderBoard Class
    The idea is to work with Hadi to make sure each player when logged in
    has a way of tracking Wins,Loses, and Experience.

---
+++ LeaderBoard.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class LeaderBoard extends JFrame
+{
+
+
+}
+
+public LeaderBoard()
+	{
+		super("Fleet Destroyer Engine");
+	}
+
+public void createLeaderBoard()
+	{
+	
+	
+	}
+
+
+
+
+
+
+public static void main(String[] args){
+         SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                new LeaderBoard().createLeaderBoard();
+
+            }
+
+        });
+   
+
+    }
+}
