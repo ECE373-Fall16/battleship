f321f2c4e9c2f6852caf9568a4f068982fb8928b
Sai Yarram <syarram@umass.edu>
2016-12-08 22:56:40 -0400

    Fixed Bugs and added a JFrame and a JPanel

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Frame;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class LeaderBoard extends JFrame
 {
 
 
-}
+
 
 public LeaderBoard()
 	{
@@ -21,7 +22,21 @@ public LeaderBoard()
 
 public void createLeaderBoard()
 	{
+	JFrame frame = new JFrame("LeaderBoard");
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	frame.setSize(500,500);
+	//frame.setBounds(0,0,1000,1000);
+	//frame.pack();
+	frame.setVisible(true);
+	JPanel contentPane = new JPanel();
+	//contentPane.setLayout();
+	contentPane.setPreferredSize(new Dimension(1500,1500));
 	
+	
+	contentPane.setBackground(Color.BLACK);
+	contentPane.setSize(400,400);
+	contentPane.setVisible(true);
+	frame.getContentPane().add(contentPane);
 	
 	}
 
@@ -30,17 +45,19 @@ public void createLeaderBoard()
 
 
 
-public static void main(String[] args){
-         SwingUtilities.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-                new LeaderBoard().createLeaderBoard();
+	public static void main(String[] args){
+			 SwingUtilities.invokeLater(new Runnable()
+			{
+				public void run()
+				{
+					new LeaderBoard().createLeaderBoard();
 
-            }
+				}
 
-        });
-   
+			});
+	   
 
-    }
+		}
+	
+
 }
