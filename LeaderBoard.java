806622ed8fa9ba7e666301178114eea0ebf72e64
Sai Yarram <syarram@umass.edu>
2016-12-08 23:59:16 -0400

    Setting up Numerous Text Areas.
    Having Problems with color but that can be played with later.

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -22,21 +22,54 @@ public LeaderBoard()
 
 public void createLeaderBoard()
 	{
+	
 	JFrame frame = new JFrame("LeaderBoard");
 	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	frame.setSize(500,500);
-	//frame.setBounds(0,0,1000,1000);
+	
 	//frame.pack();
 	frame.setVisible(true);
 	JPanel contentPane = new JPanel();
 	//contentPane.setLayout();
 	contentPane.setPreferredSize(new Dimension(1500,1500));
-	
-	
-	contentPane.setBackground(Color.BLACK);
-	contentPane.setSize(400,400);
+	//contentPane.setBackground(Color.BLACK);
+	contentPane.setOpaque(true);
 	contentPane.setVisible(true);
-	frame.getContentPane().add(contentPane);
+	contentPane.setBackground(new Color(204,204,255));
+	frame.getContentPane().setBackground(new Color(204,204,255));
+	
+	frame.add(contentPane);
+	
+	contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+	
+	JTextArea Position = new JTextArea(5,5);
+	Position.setText("Position");
+	Position.setEditable(false);
+	contentPane.add(Position);
+	JTextArea.setBackground(new Color(204,204,255));
+	
+	
+	
+	JTextArea Name = new JTextArea();
+	Name.setText("UserName");
+	Name.setEditable(false);
+	contentPane.add(Name);
+	
+	JTextArea Wins = new JTextArea();
+	Wins.setText("Wins");
+	Wins.setEditable(false);
+	contentPane.add(Wins);
+	
+	JTextArea Loses = new JTextArea();
+	Loses.setText("Loses");
+	Loses.setEditable(false);
+	contentPane.add(Loses);
+	
+	JTextArea Experience = new JTextArea();
+	Experience.setText("EXP");
+	Experience.setEditable(false);
+	contentPane.add(Experience);
+	
 	
 	}
 
