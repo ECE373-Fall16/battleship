fbbe36b6cd548355d7d2bbecee6a8b50961b217f
Sai Yarram <syarram@umass.edu>
2016-12-09 18:00:39 -0400

    Added more graphics
    Having trouble with display will have to look at this eventually.

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -8,6 +8,9 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class LeaderBoard extends JFrame
 {
@@ -26,59 +29,88 @@ public void createLeaderBoard()
 	JFrame frame = new JFrame("LeaderBoard");
 	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	frame.setSize(500,500);
+	frame.setBackground(Color.BLACK);
 	
 	//frame.pack();
 	frame.setVisible(true);
 	JPanel contentPane = new JPanel();
 	//contentPane.setLayout();
 	contentPane.setPreferredSize(new Dimension(1500,1500));
-	//contentPane.setBackground(Color.BLACK);
 	contentPane.setOpaque(true);
-	contentPane.setVisible(true);
-	contentPane.setBackground(new Color(204,204,255));
 	frame.getContentPane().setBackground(new Color(204,204,255));
-	
 	frame.add(contentPane);
+	//contentPane.setVisible(true);
+	contentPane.setBackground(Color.BLACK);
 	
 	contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 	
+	Font font = new Font("Comic Sans MS", Font.BOLD, 18);
+//Border border = BorderFactory.createLineBorder(Color.BLUE, 5);
+	
 	JTextArea Position = new JTextArea(5,5);
+	Position.setFont(font);
+	//Position.setBorder(border);
+	Position.setForeground(Color.BLUE);
 	Position.setText("Position");
 	Position.setEditable(false);
 	contentPane.add(Position);
-	JTextArea.setBackground(new Color(204,204,255));
-	
-	
-	
+	//JTextArea.setBackground(new Color(204,204,255));
+		
 	JTextArea Name = new JTextArea();
+	Name.setFont(font);
+	Name.setForeground(Color.BLUE);
 	Name.setText("UserName");
 	Name.setEditable(false);
 	contentPane.add(Name);
+	System.out.println(Name.getPreferredSize());
 	
 	JTextArea Wins = new JTextArea();
+	Wins.setFont(font);
+	Wins.setForeground(Color.BLUE);
 	Wins.setText("Wins");
 	Wins.setEditable(false);
-	contentPane.add(Wins);
+	//contentPane.add(Wins);
 	
 	JTextArea Loses = new JTextArea();
+	Loses.setFont(font);
+	Loses.setForeground(Color.BLUE);
 	Loses.setText("Loses");
 	Loses.setEditable(false);
 	contentPane.add(Loses);
 	
 	JTextArea Experience = new JTextArea();
+	Experience.setFont(font);
+	Experience.setForeground(Color.BLUE);
 	Experience.setText("EXP");
 	Experience.setEditable(false);
 	contentPane.add(Experience);
 	
-	
+	frame.pack();
+	//GetLeaderBoardValues();
 	}
+	
+public void GetLeaderBoardValues() throws Exception
+{
+	FileReader File = new FileReader("C:\\Users\\Sai Yarram\\Documents\\GitHub\\battleship\\LeaderBoard.txt");
+	BufferedReader Reader = new BufferedReader(File);
+	String text = "";
+	String line = Reader.readLine();
+	while (line!= null)
+	{
+		text+= line;
+		line = Reader.readLine();
+	}
+	System.out.println(text);
+	
+	
+}
 
 
 
 
 
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception {
 			 SwingUtilities.invokeLater(new Runnable()
 			{
 				public void run()
